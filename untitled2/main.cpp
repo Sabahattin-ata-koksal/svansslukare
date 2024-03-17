49586e128bfed2fda2b170974e7117c3e22e8838
Sabahattin-ata-koksal <156607397+Sabahattin-ata-koksal@users.noreply.github.com>
2024-03-17 20:44:15 +0300

    Update main.cpp

--- untitled2/main.cpp
+++ untitled2/main.cpp
@@ -2,19 +2,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <cctype>
 #include <ctime>
 #include "ship.h"
 #include "functions.h"
 
-
-
-void print() {
-    puts("oyunumuza hosgeldiniz");
-    puts("3 tip gemimiz bulunmaktadir hizli gemi, guclu gemi ve normal gemi hangisini secmek istersiniz");
-    puts("hizli, guclu veya normal yaziniz");
-}
 int main() {
     srand(time(NULL));
     std::string ChosenType;
@@ -22,16 +13,13 @@ int main() {
     RapidShip RShip;
     NormalShip NShip;
     HeavyShip HShip;
-
-    std::unique_ptr<MainShip> GameObject{nullptr};
-    //MainShip* GameObject= nullptr;
+    MainShip *GameObject= nullptr;
 
     print();
     std::getline(std::cin, ChosenType);
 
     ShipChoosing(ShipTypes, ChosenType);
 
-
-    SetPointer(RShip, HShip, NShip, std::move(GameObject), ShipTypes, ChosenType);
+    SetPointer(RShip, HShip, NShip, GameObject, ShipTypes, ChosenType);
     return 0;
 }
